582c9d8efab369beffbe36a2e1625eca033deaba
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-04-23 00:44:40 +0100

    Add tootips to back and forward in timeline arrows so User knows what to expect

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, IconButton, Progress } from "@chakra-ui/core";
+import { Box, IconButton, Progress, Tooltip } from "@chakra-ui/core";
+import { Tool } from "react-feather";
 
 export default function Timeline({ countItem, changeItem, data }) {
   return (
@@ -11,35 +12,45 @@ export default function Timeline({ countItem, changeItem, data }) {
       height="2rem"
       padding="2rem"
     >
-      <IconButton
-        icon="arrow-left"
-        width="10%"
-        variant="ghost"
-        onClick={() => changeItem("down")}
-        isDisabled={countItem === 0}
-        variantColor="white"
-        outline="none"
-        _focus={{
-          boxShadow: "none",
-        }}
-      />
+      <Tooltip
+        label={countItem !== 0 ? "Click to go back in time" : null}
+        placement="bottom"
+      >
+        <IconButton
+          icon="arrow-left"
+          width="10%"
+          variant="ghost"
+          onClick={() => changeItem("down")}
+          isDisabled={countItem === 0}
+          variantColor="white"
+          outline="none"
+          _focus={{
+            boxShadow: "none",
+          }}
+        />
+      </Tooltip>
       <Progress
         value={(countItem / (data.length - 1)) * 100}
         size="sm"
         width="100%"
       />
-      <IconButton
-        icon="arrow-right"
-        width="10%"
-        variant="ghost"
-        onClick={() => changeItem("up")}
-        isDisabled={countItem === data.length - 1}
-        variantColor="white"
-        outline="none"
-        _focus={{
-          boxShadow: "none",
-        }}
-      />
+      <Tooltip
+        label={countItem !== 0 ? "Click to advance in time" : null}
+        placement="bottom"
+      >
+        <IconButton
+          icon="arrow-right"
+          width="10%"
+          variant="ghost"
+          onClick={() => changeItem("up")}
+          isDisabled={countItem === data.length - 1}
+          variantColor="white"
+          outline="none"
+          _focus={{
+            boxShadow: "none",
+          }}
+        />
+      </Tooltip>
     </Box>
   );
 }
